30f6171d90401a8d320b1337add278aeec1146d3
davidczepiel <dczepiel@ucm.es>
2022-08-11 14:36:35 +0200

    Added lobby

--- Blaster/Blaster/Source/Blaster/Character/BlasterCharacter.h
+++ Blaster/Blaster/Source/Blaster/Character/BlasterCharacter.h
@@ -14,16 +14,26 @@ class BLASTER_API ABlasterCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ABlasterCharacter();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+		class USpringArmComponent* CameraBoom;
 
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+		class UCameraComponent* FollowCamera;
+
+public:
 };
